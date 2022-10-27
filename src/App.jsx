83fb2b55ab3f4e1292418c68db5766d81a0274bd
Guido Cardarelli <guidocarda@hotmail.com>
2022-10-27 00:00:07 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="min-h-screen bg-neutral-900 text-white grid place-content-center">
-      <h1 className="text-4xl">Fitness Tracker</h1>
+    <div className="min-h-screen bg-neutral-900 text-white flex items-center justify-center">
+      <div className=" rounded-md bg-neutral-800 mx-auto flex flex-col gap-4 px-4 py-8">
+        <div>
+          <h2 className="text-2xl font-bold">Push ups</h2>
+
+          <span>10</span>
+          <button className="py-2 px-4 bg-emerald-700 rounded-md font-bold">
+            add reps
+          </button>
+        </div>
+
+        <div>
+          <h2 className="text-2xl font-bold">Rows</h2>
+          <span>12</span>
+          <button className="py-2 px-4 bg-emerald-700 rounded-md font-bold">
+            add reps
+          </button>
+        </div>
+
+        <div>
+          <h2 className="text-2xl font-bold">Rows</h2>
+          <span>10</span>
+          <button className="py-2 px-4 bg-emerald-700 rounded-md font-bold">
+            add reps
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
