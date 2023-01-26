bb8ff757ad1170fb8e779fded2e2868bd60f6747
Guido Cardarelli <guidocarda@hotmail.com>
2023-01-26 09:19:26 -0300

    Revert "Edit workouts when creating them before db save"

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -10,7 +10,7 @@ const ErrorPage = () => {
       <div className="max-w-5xl min-h-screen mx-auto pt-6 px-4 border-2">
         <div className="flex flex-col items-center gap-5">
           <h1 className="text-3xl font-bold text-center">
-            {error ? error.data : "Lo sentimos! Hubo un error"}
+            Lo sentimos! Hubo un error
           </h1>
           <Link to="/" className="w-max bg-neutral-400 px-6 py-2 rounded-md">
             Volver al inicio
